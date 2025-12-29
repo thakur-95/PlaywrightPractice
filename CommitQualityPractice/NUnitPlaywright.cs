4aba23811574547b2a0d952f22cbc88032db0372
Your Name <your.email@example.com>
2025-12-29 11:09:21 +0530

    Initial commit: Playwright practice project with NUnit tests

---
+++ CommitQualityPractice/NUnitPlaywright.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace CommitQualityPractice;
+
+public class NUnitPlaywright : PageTest
+{
+    public override BrowserTypeLaunchOptions LaunchOptions
+    {
+        get
+        {
+            return new BrowserTypeLaunchOptions
+            {
+                Headless = false,
+                SlowMo = 100  // Optional: slows down operations by 100ms to see what's happening
+            };
+        }
+    }
+
+    [SetUp]
+    public async Task Setup()
+    {
+        await Page.GotoAsync("http://www.eaapp.somee.com");
+    }
+
+    [Test]
+    public async Task Test1()
+    {
+        
+        
+        
+        await Page.ClickAsync("text=Login");
+        await Page.FillAsync("#UserName", "admin");
+        await Page.FillAsync("#Password", "password");
+        await Page.ClickAsync("text=Log in");
+        await Expect(Page.Locator("text='Hello admin!'")).ToBeVisibleAsync();
+        await Task.Delay(7000);
+        
+    }
+}
---
+++ CommitQualityPractice/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿// using System.Threading.Tasks;
+// using Microsoft.Playwright;
+// using NUnit.Framework;
+// using NUnit.Framework.Internal;
+
+// namespace CommitQualityPractice;
+
+// public class Tests
+// {
+//     [SetUp]
+//     public void Setup()
+//     {
+//     }
+
+//     [Test]
+//     public async Task Test1()
+//     {
+//         //Playwright
+//         using var pw = await Playwright.CreateAsync();
+
+//         //Browser
+//         await using var browser = await pw.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+//         {
+//             Headless = false
+//         });
+
+//         //Page
+//         var page = await browser.NewPageAsync();
+//         await page.GotoAsync("http://www.eaapp.somee.com");
+//         await page.ClickAsync("text=Login");
+//         await page.ScreenshotAsync(new PageScreenshotOptions{ Path = "screenshot.png" });
+//         await page.FillAsync("#UserName", "admin");
+//         await page.FillAsync("#Password", "password");
+//         await page.ClickAsync("text=Log in");
+//         var isExist = await page.Locator("text='Hello admin!'").IsVisibleAsync();
+//         Assert.That(isExist, Is.True);
+
+//     }
+// }
